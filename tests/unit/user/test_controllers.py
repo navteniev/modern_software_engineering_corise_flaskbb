093b470f967a984acdc2b15d7c734d0f87a25f14
Laura Webber <laura@laurawebber.com>
2023-06-16 16:34:26 +0000

    edit tests

--- tests/unit/user/test_controllers.py
+++ tests/unit/user/test_controllers.py
@@ -276,10 +276,8 @@ class TestChangeUserDetailsView(object):
         view.get()
 
     def test_update_user_details_successfully_updates(self, user, mocker):
-        # CoRise TODO: fix this test so that it passes.
-        # Hint: does the format of the data look right to you?
         form = self.produce_form(
-            birthday="25 04 2000",
+            birthday="2000-04-25",
             gender="awesome",
             location="here",
             website="http://web.site",
@@ -319,8 +317,7 @@ class TestChangeUserDetailsView(object):
         assert form.errors == {"birthday": ["Not a valid date value."]}
 
     def test_update_user_fails_with_stopvalidation(self, mocker):
-        # CoRise TODO: fix this test so that it passes.
-        form = self.produce_form(birthday="25 04 2000")
+        form = self.produce_form(birthday="2000-04-25")
         handler = mocker.Mock(spec=ChangeSetHandler)
         handler.apply_changeset.side_effect = StopValidation(
             [("birthday", "I just want you to know that's a great birthday")]
@@ -334,8 +331,7 @@ class TestChangeUserDetailsView(object):
         }
 
     def test_update_user_fails_with_persistence_error(self, mocker):
-        # CoRise TODO: fix this this test so that it passes.
-        form = self.produce_form(birthday="25 04 2000")
+        form = self.produce_form(birthday="2000-04-25")
         handler = mocker.Mock(spec=ChangeSetHandler)
         handler.apply_changeset.side_effect = PersistenceError("no")
         view = ChangeUserDetails(form=form, details_update_handler=handler)
