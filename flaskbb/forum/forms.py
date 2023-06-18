6d52c5942bdfaef06c8f47024ce01c24dc78d287
mannyoliv15 <80927306+mannyoliv15@users.noreply.github.com>
2023-06-18 05:41:21 +0000

    Week 1 Changes

--- flaskbb/forum/forms.py
+++ flaskbb/forum/forms.py
@@ -111,8 +111,11 @@ class TopicForm(FlaskForm):
 # you modify them?
 
 class SpecialTopicForm(TopicForm):
-    # ADD CODE HERE
-    pass
+    def __init__(self, *args, **kwargs):
+       TopicForm.__init__(self, *args, **kwargs)
+       self.title.process_data(f'Special Topic: {self.title.data}')
+       self.content.process_data(f'Special Topic: {self.content.data}')
+   
 
 ###################################################################
 
