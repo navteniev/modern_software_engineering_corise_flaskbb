856be677067d57e40db196fbaa6456553b8d423d
mannyoliv15 <80927306+mannyoliv15@users.noreply.github.com>
2023-06-18 06:00:27 +0000

    Week 1 Changes second attempt

--- tests/endtoend/test_flaskbb.py
+++ tests/endtoend/test_flaskbb.py
@@ -9,17 +9,22 @@ from playwright.sync_api import Page
 
 @pytest.fixture
 def app():
-    # Hint: create the app, and setup any default context like translations,
-    # settings, DB, etc.
-    # Hint: take a look at the tests/fixtures/app.py file for the details of 
-    # how to configure the application.
-    # TODO: ADD CODE HERE
-    pass
+    """application with context."""
+    app = create_app(Config)
+    
+    with app.app_context():
+        db.create_all()
+        create_default_groups()
+        create_default_settings()
+        compile_translations()
 
-def test_load_home_page(live_server, page: Page):
-    # Hint: Check out `flask.url_for` helper function to get the external url for 
-    # an endpoint. Then go to it using playwright's `page.goto(url)`
-    # TODO: ADD CODE HERE
-    pass
+    return app
+
+def test_create_new_account(live_server, page: Page):    
+    url=url_for('forum.index', _external=True)
+
+    page.goto(url)
+    expect(page).to_have_title("FlaskBB - A lightweight forum software in Flask")
+    
 
 ###################################################################
