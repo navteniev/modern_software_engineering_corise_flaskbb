0a4b4ee1be6f2a4ab244128fba445f1bb748d476
Nikolai Avteniev <nikolai.avteniev@gmail.com>
2023-07-02 19:08:33 +0000

    Added dashboard template

--- terraform/cloud_watch.tf
+++ terraform/cloud_watch.tf
@@ -1,1 +1,8 @@
 # CoRise TODO: create a new log group
+
+# CoRise TODO: create a new dashboard
+resource "aws_cloudwatch_dashboard" "flaskbb_main" {
+  dashboard_name = "flaskbb_main"
+
+  dashboard_body = jsonencode({})
+}
