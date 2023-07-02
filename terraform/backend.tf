3dadf5c17f6912d6c783636edb03c4fb951d2fde
mannyoliv15 <80927306+mannyoliv15@users.noreply.github.com>
2023-07-02 20:58:26 +0000

    Week 3 Edits

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-flaskbb"
+    bucket = "terraform-state-flaskbb-mannyoliv15"
     key    = "core/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
