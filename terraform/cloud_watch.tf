922bdbb148e4a2939ce94d2158730499dee5be51
Nikolai Avteniev <nikolai.avteniev@gmail.com>
2023-07-02 19:41:00 +0000

    Added metric alarm template

--- terraform/cloud_watch.tf
+++ terraform/cloud_watch.tf
@@ -6,3 +6,16 @@ resource "aws_cloudwatch_dashboard" "flaskbb_main" {
 
   dashboard_body = jsonencode({})
 }
+
+#CoRise TODO: create metric alters
+resource "aws_cloudwatch_metric_alarm" "bot_attack" {
+  alarm_name                = "bot_attack"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 2
+}
+
+resource "aws_cloudwatch_metric_alarm" "too_busy" {
+  alarm_name                = "too_busy"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  evaluation_periods        = 2
+}
